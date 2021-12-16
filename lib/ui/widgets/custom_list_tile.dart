c0ad4892915cb3de01f487a99d49f5b98d0effe1
misterrio <misterriobit@gmail.com>
2021-12-16 11:29:04 +0300

    Реализовали карточку персонажа

--- lib/ui/widgets/custom_list_tile.dart
+++ lib/ui/widgets/custom_list_tile.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty_new/data/models/character.dart';
+import 'package:rick_and_morty_new/ui/widgets/characer_status.dart';
 
 class CustomListTile extends StatelessWidget {
   final Results result;
@@ -7,10 +11,102 @@ class CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        result.name,
-        style: const TextStyle(color: Colors.white),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 7,
+        color: const Color.fromRGBO(86, 86, 86, 0.8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CachedNetworkImage(
+              imageUrl: result.image,
+              placeholder: (context, url) => const Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: CircularProgressIndicator(
+                  color: Colors.grey,
+                ),
+              ),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.9,
+                      child: Text(
+                        result.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyText2,
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CharacterStatus(
+                      liveState: result.status == 'Alive'
+                          ? LiveState.alive
+                          : result.status == 'Dead'
+                              ? LiveState.dead
+                              : LiveState.unknown),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: Row(
+                      //Row for Species and Gendor
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          //Species column
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Species:',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              result.species,
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          //Gender column
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Gender:',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              result.gender,
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
